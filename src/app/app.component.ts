25f6780ab3ea187659627200dc643d91a7528bd7
Denise Schmidt <rickylee4388@web.de>
2023-09-25 22:04:06 +0200

    insert img start animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'deniseschmidt';
+export class AppComponent implements OnInit {
+  images =['cabbage.jpg','cucumber-plant.jpg','onion.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit(){
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(()=>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(()=>{
+        this.showImage = true;
+      },10);
+    },8000);
+  }
 }
