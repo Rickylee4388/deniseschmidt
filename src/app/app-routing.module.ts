64f9d01d2b3ae7728ae98f62810ed9c5fb8f49fb
Denise Schmidt <rickylee4388@web.de>
2023-09-28 19:53:23 +0200

    imprint&dataprotection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImprintComponent } from '../app/imprint/imprint.component';
+import { SlideshowComponent } from '../app/slideshow/slideshow.component';
+import { DataProtectionComponent } from '../app/data-protection/data-protection.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SlideshowComponent },
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'data-protection', component: DataProtectionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
